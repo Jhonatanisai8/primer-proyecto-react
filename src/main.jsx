a21451c0f88acbd15b0ebe36b0d59614b0b7f6f4
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-09-17 14:47:50 -0500

    estilos css

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import { PrimerComponente } from "./01-variables/PrimerComponente";
 import { SegundoComponente } from "./01-variables/SegundoComponente";
 import { ComponenteVariables } from "./01-variables/ComponenteVariables";
-
+// importamos los estilos css
+import "./styles.css";
+import "./01-variables/PrimerComponente.css"
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <SegundoComponente /> */}
-    {/* <PrimerComponente /> */}
-    <ComponenteVariables />
+    <PrimerComponente />
+    {/* <ComponenteVariables /> */}
   </StrictMode>
 );
